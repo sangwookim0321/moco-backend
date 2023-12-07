498ab5bc56add093580e96aa231d95a96956624f
하디 <pointjumpit@coinvest.kr>
2023-12-07 17:56:06 +0900

    질문 등록 api 추가

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const db = require('../../models/db.js')
 
-router.post('/add', (req, res) => {
+router.post('/addTest', (req, res) => {
 	const { testName, testDescription } = req.body
 
 	if (!testName || !testDescription) {
@@ -23,12 +23,43 @@ router.post('/add', (req, res) => {
 			})
 		})
 		.catch((err) => {
-			console.error('Error adding test: ', err)
+			console.error('/addTest Error : ', err)
 			res.status(500).json({
 				status: 'error',
-				message: '테스트 추가 중 오류가 발생했습니다.',
+				message: '테스트 추가 중 서버 오류가 발생했습니다.',
 			})
 		})
+})
+
+router.post('/addQuestion', async (req, res) => {
+	const { testId, contents } = req.body
+
+	if (!testId || !contents || !Array.isArray(contents) || contents.length === 0) {
+		return res.status(400).json({
+			status: 'error',
+			message: '필수 항목을 모두 입력해주세요.',
+		})
+	}
+
+	try {
+		// 각 질문에 대해 데이터베이스에 삽입
+		for (const content of contents) {
+			const query = 'INSERT INTO psych_tests.Questions (test_id, content) VALUES ($1, $2)'
+			const values = [testId, content]
+			await db.query(query, values)
+		}
+
+		res.status(201).json({
+			status: 'success',
+			message: '질문이 성공적으로 추가되었습니다.',
+		})
+	} catch (err) {
+		console.error('/addQuestion Error : ', err)
+		res.status(500).json({
+			status: 'error',
+			message: '질문 추가 중 서버 오류가 발생했습니다.',
+		})
+	}
 })
 
 module.exports = router
