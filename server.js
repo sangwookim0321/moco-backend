5f7c7eb837f6c7d79635f1bebe6f839c8930aeac
하디 <pointjumpit@coinvest.kr>
2023-12-11 09:33:02 +0900

    default port 설정

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const dotenv = require("dotenv");
-const express = require("express");
-const cors = require("cors");
-const app = express();
+const dotenv = require('dotenv')
+const express = require('express')
+const cors = require('cors')
+const app = express()
 
-if (process.env.NODE_ENV === "production") {
-  dotenv.config({ path: "./.env.production" });
+if (process.env.NODE_ENV === 'production') {
+	dotenv.config({ path: './.env.production' })
 } else {
-  dotenv.config({ path: "./.env.development" });
+	dotenv.config({ path: './.env.development' })
 }
 
-const PORT = process.env.PORT;
-const swaggerUi = require("swagger-ui-express");
-const swaggerFile = require("./swagger-output.json");
+const PORT = process.env.PORT || 3000
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger-output.json')
 
-app.use(express.json());
-app.use(cors());
-app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use(express.json())
+app.use(cors())
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
-const adminRouter = require("./routes/admin/admin.js");
-app.use("/admin", adminRouter);
+const adminRouter = require('./routes/admin/admin.js')
+app.use('/admin', adminRouter)
 
 app.listen(PORT, (req, res) => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+	console.log(`Server is running on port ${PORT}.`)
+})
