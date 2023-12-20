6fffe2c3ed6e919767ee2b0df418bce0aab00e1e
하디 <pointjumpit@coinvest.kr>
2023-12-20 09:00:26 +0900

    process.on 에러 처리

--- server.js
+++ server.js
@@ -22,6 +22,36 @@ const userRouter = require('./routes/user/user.js')
 app.use('/admin', adminRouter)
 app.use('/user', userRouter)
 
+process.on('uncaughtException', (err) => {
+	console.error('uncaughtException 발생 : ', err)
+
+	if (err.code === 'EADDRINUSE') {
+		console.error('포트가 이미 사용 중입니다.')
+		process.exit(1)
+	} else if (err.code === 'EACCES') {
+		console.error('권한이 없습니다.')
+		process.exit(1)
+	} else if (err.code === 'ECONNREFUSED') {
+		console.error('DB에 연결할 수 없습니다.')
+		process.exit(1)
+	} else if (err.code === 'ENOTFOUND') {
+		console.error('DB에 연결할 수 없습니다.')
+		process.exit(1)
+	} else if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+		console.error('DB 연결이 끊어졌습니다.')
+		process.exit(1)
+	} else if (err.code === 'ER_CON_COUNT_ERROR') {
+		console.error('DB 연결이 너무 많습니다.')
+		process.exit(1)
+	} else if (err.code === 'ER_ACCESS_DENIED_ERROR') {
+		console.error('DB에 접근할 수 없습니다.')
+		process.exit(1)
+	} else {
+		console.error('알 수 없는 에러가 발생했습니다.')
+		process.exit(1)
+	}
+})
+
 app.listen(PORT, (req, res) => {
 	console.log(`Server is running on port ${PORT}.`)
 })
